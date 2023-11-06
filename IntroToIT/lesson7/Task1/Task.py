cf606a254b975fca19724f03bb0cb010fed6e759
Angelina <lina.senina.07@mail.ru>
2023-11-06 20:19:00 +0300

    refactor:code recycling

--- IntroToIT/lesson7/Task1/Task.py
+++ IntroToIT/lesson7/Task1/Task.py
@@ -1,21 +1,23 @@
-#INTRO TO IT 2nd COURSE
-def add_numbers(a, b):
-
+# Функция для сложения двух чисел
+def add_numbers(a, b):      
     result = a + b
     return result
 
-def multiply_numbers(a, b):
 
+# Функция для умножения двух чисел
+def multiply_numbers(a, b):
     result = a * b
     return result
 
-def find_max_number(numbers):
 
+# Функция для нахождения максимального числа в списке
+def find_max_number(numbers):
     max_number = max(numbers)
     return max_number
 
+
+# Функция для вычисления факториала числа
 def calculate_factorial(n):
- 
     if n == 0:
         return 1
     factorial = 1
@@ -23,13 +25,16 @@ def calculate_factorial(n):
         factorial *= i
     return factorial
 
-def is_even(number):
 
+# Функция для определения четности числа
+def is_even(number):
     if number % 2 == 0:
         return True
     else:
         return False
 
+
+# Определение чисел и вызов функций
 num1 = 10
 num2 = 5
 sum_result = add_numbers(num1, num2)
@@ -39,10 +44,13 @@ max_num = find_max_number(numbers_list)
 factorial_result = calculate_factorial(5)
 is_even_num = is_even(7)
 
+# Вывод результатов
 print(f"Сумма чисел {num1} и {num2} равна {sum_result}")
 print(f"Произведение чисел {num1} и {num2} равно {product_result}")
 print(f"Наибольшее число в списке {numbers_list} - {max_num}")
 print(f"Факториал числа 5 равен {factorial_result}")
+
+# Проверка четности числа
 if is_even_num:
     print("Число 7 - четное.")
 else:
