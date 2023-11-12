46dbe4d924d2b349617c020da78007b61a72c57e
Angelina <lina.senina.07@mail.ru>
2023-11-12 16:59:30 +0300

    fix: убрала ошибки

--- IntroToIT/lesson3/Task10/Task.py
+++ IntroToIT/lesson3/Task10/Task.py
@@ -1,6 +1,6 @@
 #INTRO TO IT 2nd COURSE
 s = input("Введите строку: ").replace(' ', '').lower()
-if s = s[::-1]:
+if s == s[::-1]:
     print("Строка является палиндромом")
 else:
     print("Строка не является палиндромом")
