74ea73f48c36540da90481de87e64c543a247609
Angelina <lina.senina.07@mail.ru>
2023-11-17 13:03:01 +0300

    fix:тестировка

--- IntroToIT/lesson6/Task5/Task.py
+++ IntroToIT/lesson6/Task5/Task.py
@@ -3,4 +3,6 @@
 #(Предположим, что это простой кредит без процентов)
 
 def iphone_credit_cost(iphone_price, months):
-    return iphone_price ** months
+    monthly_payment = iphone_price / months
+    total_cost = monthly_payment * months
+    return total_cost
